abf9f49c771fd30a63437894d056332a737d69ff
SamTo83 <Sam2thanh@hotmail.co.uk>
2021-11-24 17:03:28 +0000

    commit

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -3,9 +3,13 @@
 var input = document.querySelector('#input');
 var submitBtn = document.querySelector('.plusButton');
 var taskList = document.querySelector('.task__description');
+var resetBtn = document.querySelector('.resetButton');
 
 var createTask = function createTask() {
   taskList.innerHTML += "<li><input id=\"check\" type=\"checkbox\">".concat(input.value, " <i class=\"fas fa-pen\"></i></li>");
 };
 
 submitBtn.addEventListener("click", createTask);
+resetBtn.addEventListener('click', function () {
+  taskList.innerHTML = "";
+});
--- main.js
+++ main.js
@@ -3,9 +3,14 @@
 const input = document.querySelector('#input');
 const submitBtn = document.querySelector('.plusButton');
 const taskList = document.querySelector('.task__description');
+const resetBtn = document.querySelector('.resetButton');
 
 const createTask = () => {
   taskList.innerHTML += `<li><input id="check" type="checkbox">${input.value} <i class="fas fa-pen"></i></li>`
 }
 
 submitBtn.addEventListener("click", createTask);
+
+resetBtn.addEventListener('click', () => {
+  taskList.innerHTML = "";
+})
